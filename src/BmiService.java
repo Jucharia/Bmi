bd9c89d98bbcac6af8ccf4d6b91639a86cefd65c
Yuliya Mylnikova <jucharia87@gmail.com>
2023-02-25 17:00:43 +0300

    Math.pow

--- src/BmiService.java
+++ src/BmiService.java
@@ -3,7 +3,10 @@ public class BmiService {
         double x;
         weight = 62;
         height = 1.55;
-        x = weight / (height * height);
+        double a1 = height;
+        double b1 = 2;
+        double y = Math.pow(a1, b1); // возведение числа в квадрате(рост)
+        x = weight / y;
         int result = (int) x;
         return result;
 
